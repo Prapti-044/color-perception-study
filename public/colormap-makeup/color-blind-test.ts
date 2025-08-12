6e58ab3ee5e50f1e650543d91e51b4f36448146b
Shadmaan Hye <praptishadmaan@gmail.com>
2025-08-12 15:37:26 -0600

    not working MRE

--- public/colormap-makeup/color-blind-test.ts
+++ public/colormap-makeup/color-blind-test.ts
@@ -4,7 +4,8 @@ console.log("Rahat")
 
 export default function func({ answers }: JumpFunctionParameters<{ name: string }>): JumpFunctionReturnVal {
     console.log(answers);
+    console.log("This is a Test. This is not printing!");
 
     return { component: "color-blind-failed-component" };
-    // return { component: null };
+    // return { component: null }; // This also does not work
 }
