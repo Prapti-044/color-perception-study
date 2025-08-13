e68ba26ee2072844677c5c404e99774bf87432d6
Shadmaan Hye <praptishadmaan@gmail.com>
2025-08-13 10:02:39 -0600

    files update

--- public/colormap-makeup/color-blind-test.ts
+++ public/colormap-makeup/color-blind-test.ts
@@ -1,10 +1,7 @@
 import { JumpFunctionParameters, JumpFunctionReturnVal } from '../../src/store/types';
 
-console.log("Rahat")
-
 export default function func({ answers }: JumpFunctionParameters<{ name: string }>): JumpFunctionReturnVal {
-    console.log(answers);
-    console.log("This is a Test. This is not printing!");
+    console.log("This is not printing!");
 
     return { component: "color-blind-failed-component" };
     // return { component: null }; // This also does not work
